a21d4742937e9f21715eeb1fe71d468ca19261c7
babie <babie.wildheart@gmail.com>
2023-02-17 15:19:54 +0900

    feat: add blog index and detail page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const selfConfig = require('./self.config.json');
+import { defineConfig } from 'astro/config'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const selfConfig = require('./self.config.json')
 
 // https://astro.build/config
-export default defineConfig({ ...selfConfig });
+export default defineConfig({ trailingSlash: 'never', ...selfConfig })
--- src/content/blog/TEST.md
+++ src/content/blog/TEST.md
@@ -1,3 +1,12 @@
+---
+title: TEST
+lang: ja
+draft: false
+createdAt: '2023-01-01T01:01:01.001Z'
+updatedAt: '2023-01-02T01:02:02.002Z'
+publishedAt: '2023-01-03T03:03:03.003Z'
+---
+
 # TEST
 
 This is Test.
